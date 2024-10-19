03fcdcd0d724d95875a260fc9d2ac3377460b868
MatherTGL <69119562+MatherTGL@users.noreply.github.com>
2024-10-19 14:37:51 +0500

    Update WorldTime.cs

--- Get World Time/WorldTime.cs
+++ Get World Time/WorldTime.cs
@@ -1,25 +1,51 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace GameAssets.Scripts.Service.Time
 {
     public static class WorldTime
     {
+        private static DateTime? cachedTime;
+
+        private static DateTime lastFetchTime;
+
+        private static TimeSpan cacheDuration = TimeSpan.FromMinutes(10);
+
+
         public static async Task<DateTime> GetAsync()
         {
-            await Task.Run(() =>
-            {
-                HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create("http://www.microsoft.com");
-                WebResponse response = myHttpWebRequest.GetResponseAsync().Result;
+            if (cachedTime.HasValue && DateTime.UtcNow - lastFetchTime < cacheDuration)
+                return cachedTime.Value;
 
-                string todaysDates = response.Headers["date"];
-                return DateTime.ParseExact(todaysDates,
-                    "ddd, dd MMM yyyy HH:mm:ss 'GMT'",
-                    CultureInfo.InvariantCulture.DateTimeFormat,
-                    DateTimeStyles.AssumeUniversal);
-            });
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromSeconds(10);
+                    HttpResponseMessage response = await client.GetAsync("http://www.microsoft.com");
+                    response.EnsureSuccessStatusCode();
+
+                    IEnumerable<string> values;
+                    if (response.Headers.TryGetValues("date", out values))
+                    {
+                        string todaysDate = values.First();
+                        return DateTime.ParseExact(todaysDate,
+                            "ddd, dd MMM yyyy HH:mm:ss 'GMT'",
+                            CultureInfo.InvariantCulture.DateTimeFormat,
+                            DateTimeStyles.AssumeUniversal);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
 
             return GetLocal();
         }
