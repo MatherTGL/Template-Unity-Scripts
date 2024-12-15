a0e2b320f5812f28103abdfdb12982d7bff01f19
MatherTGL <69119562+MatherTGL@users.noreply.github.com>
2024-12-15 14:50:28 +0500

    last

---
+++ WebGL/DeepLinking/DeepLinkChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace GameAssets.Scripts.Utils.DeepLinking
+{
+    public static class DeepLinkChecker
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        private static extern IntPtr GetDeepLink();
+#endif
+
+        public static string GetDeepLinkArgument()
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            IntPtr strPtr = GetDeepLink();
+            return Marshal.PtrToStringUTF8(strPtr);
+#else
+            return "";
+#endif
+        }
+
+        public static bool HasDeepLinkArgument()
+        {
+            return !string.IsNullOrEmpty(GetDeepLinkArgument());
+        }
+    }
+}
---
+++ WebGL/DeepLinking/DeepLinkChecker.jslib
@@ -0,0 +1,9 @@
+﻿mergeInto(LibraryManager.library, {
+    GetDeepLink: function () {
+        var returnStr = window.Telegram.WebApp.initDataUnsafe.start_param || "";
+        var bufferSize = lengthBytesUTF8(returnStr) + 1;
+        var buffer = _malloc(bufferSize);
+        stringToUTF8(returnStr, buffer, bufferSize);
+        return buffer;
+    }
+});
